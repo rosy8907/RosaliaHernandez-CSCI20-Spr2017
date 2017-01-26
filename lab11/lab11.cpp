e06904a4a1782ce3df5c4ed9919becbafb2eb05c
Rosy Hernandez <rhernandez007@student.butte.edu>
2017-01-26 22:57:20 +0000

    lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,24 @@
+/*
+This is a numbers guessing game. 
+The numbers are 1 through 50.
+The computer will choose a random number from 1 through 50.
+The computer will display all numbers from 1 through 50 for the user to guess.
+The computer will display the text "choose a number 1 through 50"
+The user has three clicks to guess the number the computer has chosen.
+The user has 10 points on the scoreboard, the goal is to play as long as possible without losing all 10 points.
+The user will click on the number that they think the computer has chosen. 
+If the number they chose is lower than the number the computer has chosen,
+the computer will inform the user that the number is higher and to try again.
+If the number they chose is higher then the number the computer has chosen,
+the computer will inform the user that the number is lower, and to try again.
+If the number they chose is the number the computer has chosen, they win and the scoreboard remains the same.
+The game restarts. 
+If the number they chose is the number the computer has not chosen and they have 
+tried to guess the number three times, the computer wins and 1 point from 10 is deducted from the users scoreboard.
+The game restarts.
+When the scoreboard reaches 0, the game restarts with 10 points on the scoreboard. */
+
+git add lab11
+git commit -a -m 'lab1.1 submission'
+git push
+

deb074bcd76ff7287be4adc4d0506f2259eb5d19
Rosy Hernandez <rhernandez007@student.butte.edu>
2017-04-11 18:17:28 +0000

    Lab 3.3 Submission

--- lab33/lab33.cpp
+++ lab33/lab33.cpp
@@ -14,34 +14,34 @@ int randomNumber () {                                                           
 }
 
 int main (){
-    int computerGuess = 0;
+    int computerGuess = 0;                                                      
     int userGuess = 0;
     int isTrue = 1;
     
-    computerGuess = randomNumber();
+    computerGuess = randomNumber();                                             //computer choses a random number between 1 and 50
 
 cout << "Guess the number chosen between 1 and 50:" << endl;
     
-while (isTrue == 1) {
+while (isTrue == 1) {                                                           //while loop keeps the numbers being guessed between 1 and 50
     
-    cin >> userGuess; 
-    if (userGuess < 0 || userGuess > 50) {
+    cin >> userGuess;                                                           //accepts user input inside the while loop 
+    if (userGuess < 0 || userGuess > 50) {                                      //conditions user input value
         cout << "Oops, try a number between 1 and 50" << endl;
         isTrue = 1;
 
     }
-    if (userGuess > 0 && userGuess < 51 && userGuess < computerGuess) {
+    if (userGuess > 0 && userGuess < 51 && userGuess < computerGuess) {         //if user's guess is higher, "too low"
         cout << "Too Low" << endl;
         isTrue = 1;
     }
-    if (userGuess > 0 && userGuess < 51 && userGuess > computerGuess) {
+    if (userGuess > 0 && userGuess < 51 && userGuess > computerGuess) {         //if user's guess is lower, "too high"
         cout << "Too High" << endl;
         isTrue = 1;
     }
-    if (userGuess == computerGuess) {
+    if (userGuess == computerGuess) {                                           //end og guessing game when user choses the right number
         cout << "Just Right" << endl;
         cout << "Goodbye." << endl;
-        isTrue = 0;
+        isTrue = 0;                                                             //condition for while loop to terminate
         
     }
     
